0517e9a16ec7043819d734e9ccb51fdd196e6eda
Michael Bukachi <michaelbukachi@gmail.com>
2019-12-31 13:11:14 +0300

    feat: Add core functionality

---
+++ flask_authlib_client/__init__.py
@@ -0,0 +1,36 @@
+from authlib.integrations.flask_oauth2 import ResourceProtector
+
+from flask_authlib_client.extras import RemoteToken, RemoteTokenValidator
+
+
+class AuthClient(object):
+
+    def __init__(self, app=None, oauth=None, token_class=None):
+        """
+        Constructor for flask_authlib_client.AuthClient
+
+        :param app: Flask application
+        """
+        self._require_auth = None
+
+        if app is not None:
+            self.init_app(app, oauth, token_class)
+
+    def init_app(self, app, oauth, token_class=RemoteToken):
+        """
+        Configure AuthClient for Flask application
+
+        Available configuration options:
+          RESOURCE_SERVER_INTROSPECTION_URL
+          RESOURCE_SERVER_INTROSPECTION_CREDENTIALS
+        :param app: Flask application
+        :param oauth: Oauth instance
+        :param token_class: a token class which inherits from TokenMixin
+        """
+        require_oauth = ResourceProtector()
+        require_oauth.register_token_validator(RemoteTokenValidator(token_class))
+        self._require_auth = require_oauth
+
+    @property
+    def require_oauth(self):
+        return self._require_auth
---
+++ flask_authlib_client/extras.py
@@ -0,0 +1,50 @@
+import requests
+from authlib.oauth2.rfc6749 import TokenMixin, time
+from authlib.oauth2.rfc6750 import BearerTokenValidator
+from flask import current_app
+
+
+class RemoteTokenValidator(BearerTokenValidator):
+
+    def __init__(self, token_cls, realm=None):
+        super().__init__(realm)
+        self.token_cls = token_cls
+
+    def authenticate_token(self, token_string):
+        url = current_app.config['RESOURCE_SERVER_INTROSPECTION_URL']
+        credentials = current_app.config['RESOURCE_SERVER_INTROSPECTION_CREDENTIALS']
+        res = requests.post(url, data={'token': token_string}, auth=credentials)
+        if res.ok:
+            return self.token_cls(res.json())
+
+        return None
+
+    def request_invalid(self, request):
+        return False
+
+    def token_revoked(self, token):
+        return token.is_revoked()
+
+
+class RemoteToken(TokenMixin):
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_client_id(self):
+        return self.token.get('client_id', None)
+
+    def get_scope(self):
+        return self.token.get('scope', None)
+
+    def get_expires_in(self):
+        return self.token.get('exp', 0)
+
+    def get_expires_at(self):
+        expires_at = self.get_expires_in() + self.token.get('iat', 0)
+        if expires_at == 0:
+            expires_at = time.time() + 3600  # Expires in an hour
+        return expires_at
+
+    def is_revoked(self):
+        return self.token.get('active', False)
